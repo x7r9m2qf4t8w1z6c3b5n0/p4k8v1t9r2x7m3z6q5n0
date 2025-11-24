dfb6433a6858b4e9c825df7a62af2797b23177ad
x7r9m2qf4t8w1z6c3b5n0 <engineeredawareness@gmail.com>
2025-11-24 23:07:00 +0600

    Add files via upload

---
+++ data/user1.js
@@ -0,0 +1,70 @@
+const subjectsData = [
+            {
+                name: "Mentorship Session",
+                icon: "fas fa-chalkboard-teacher",
+                color: "#9b59b6",
+                isPCMB: false,
+                lectures: [
+                ]
+            },
+            {
+                name: "Physics",
+                icon: "fas fa-atom",
+                color: "#ff6b6b",
+                isPCMB: true,
+                lectures: [
+{ title: "তাপগতিবিদ্যা (পর্ব ১)", videoId: "nxVrNvcF1gg" }
+                ]
+            },
+            {
+                name: "Chemistry",
+                icon: "fas fa-flask",
+                color: "#4ecdc4",
+                isPCMB: true,
+                lectures: [
+
+                ]
+            },
+            {
+                name: "Mathematics",
+                icon: "fas fa-square-root-alt",
+                color: "#45b7d1",
+                isPCMB: true,
+                lectures: [
+
+                ]
+            },
+            {
+                name: "Biology",
+                icon: "fas fa-dna",
+                color: "#96ceb4",
+                isPCMB: true,
+                lectures: [
+
+                ]
+            },
+            {
+                name: "English",
+                icon: "fas fa-book",
+                color: "#feca57",
+                isPCMB: false,
+                lectures: [
+                ]
+            },
+            {
+                name: "Bangla",
+                icon: "fas fa-language",
+                color: "#ff9ff3",
+                isPCMB: false,
+                lectures: [
+                ]
+            },
+            {
+                name: "ICT",
+                icon: "fas fa-laptop-code",
+                color: "#54a0ff",
+                isPCMB: false,
+                lectures: [
+                ]
+            }
+        ];
