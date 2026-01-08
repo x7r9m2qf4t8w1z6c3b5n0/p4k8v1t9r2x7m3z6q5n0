f360fe3f3b5dcd69d87d9f6d6dcd7a0c165ca376
x7r9m2qf4t8w1z6c3b5n0 <engineeredawareness@gmail.com>
2026-01-08 21:21:01 +0600

    Update cache1.js

--- data/cache1.js
+++ data/cache1.js
@@ -169,38 +169,4 @@ const subjectsData = [
 { title: "Physics Mentorship Session", videoId: "TkVrn8T-ZlU" },
                 ]
             },
-{
-    name: "Organic Zero to Pro",
-    icon: "fas fa-flask",
-    color: "#f4b400",
-    isPCMB: true,
-    lectures: [
-        { title: "Lecture 1", videoId: "RLpWLi4hQVQ" },
-        { title: "Lecture 2", videoId: "t-ve2X2lyac" },
-        { title: "Lecture 3", videoId: "j_I9FHVyjIc" },
-        { title: "Lecture 4", videoId: "0Q5FomlQfUI" },
-        { title: "Lecture 5", videoId: "6SaH4EHCDWU" },
-        { title: "Lecture 6", videoId: "cI1PWesMhHQ" },
-        { title: "Lecture 7", videoId: "OuA6iFCIINE" },
-        { title: "Lecture 8", videoId: "JoTGJcv8HiU" },
-        { title: "Lecture 9", videoId: "lPh4o2zD7Nw" },
-        { title: "Lecture 10", videoId: "d2g6rjoL9ZE" },
-        { title: "Lecture 11", videoId: "3pYNPUjiGP0" },
-        { title: "Lecture 12", videoId: "044u2oseQ8g" },
-        { title: "Lecture 13", videoId: "kF0tWWnoIig" },
-        { title: "Lecture 14", videoId: "rI6paeInMxI" },
-        { title: "Lecture 15", videoId: "o0gYzdm1WbI" },
-        { title: "Lecture 16", videoId: "h3lhrN8Duao" },
-        { title: "Lecture 17", videoId: "3e27RhYr8bE" },
-        { title: "Lecture 18", videoId: "xhqRxuvWPdE" },
-        { title: "Lecture 19", videoId: "kUUz_efGZtM" },
-        { title: "Lecture 20", videoId: "GAC1KwWlwRk" },
-        { title: "Lecture 21", videoId: "-gsGJw2hAgQ" },
-        { title: "Lecture 22", videoId: "JzpWn3M-RX8" },
-        { title: "Lecture 23", videoId: "CQ6jwITRhk8" },
-        { title: "Lecture 24", videoId: "Z_9Ut-TZL_A" },
-        { title: "Lecture 25", videoId: "nvxw39JWDBY" },
-        { title: "Lecture 26", videoId: "eXD2NrI4FEo" }
-    ]
-},
         ];
